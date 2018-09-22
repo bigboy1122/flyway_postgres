fd2a2c6c0fd01a9bce4227663e1f50b4d0bd8c11
Anthony Galati <tgalati@naic.org>
2018-09-22 07:22:15 -0500

    Initial commit

---
+++ flyway/sql/V2__seed_data.sql
@@ -0,0 +1,3 @@
+Insert into bar.person (person_id,last_name,first_name) values (1,'Jenkins','Leroy');
+Insert into bar.person (person_id,last_name,first_name) values (2,'Ford','Harrison');
+Insert into bar.person (person_id,last_name,first_name) values (3,'Smith','John');
